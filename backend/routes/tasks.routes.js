ffbd0a7caf7e1bc96643d305495e00b3d6e4d765
JOOSEF-7 <jooshetaia160@gmail.com>
2026-06-15 22:40:55 +0300

    add manger role to tasks routes

--- backend/routes/tasks.routes.js
+++ backend/routes/tasks.routes.js
@@ -22,20 +22,34 @@ import upload from "../Middleware/multerConfig.js";
 import { processUploadedFile2 } from "../Middleware/processUploads2.js";
 import { setFilesToBody2 } from "../Middleware/setFilesToBody2.js";
 
-router.route("/task-stats").get(verifyToken, allowedTo("EMPLOYEE", "HR"), getTaskStatistics);
-router.route("/search").get(verifyToken, allowedTo("HR", "EMPLOYEE"), searchTasks);
-router.route("/my-tasks").get(verifyToken, allowedTo("EMPLOYEE"), getMyAndTeamTasks);
+router
+    .route("/task-stats")
+    .get(
+        verifyToken,
+        allowedTo("EMPLOYEE", "HR", "MANAGER"),
+        getTaskStatistics
+    );
+router
+    .route("/search")
+    .get(verifyToken, allowedTo("HR", "EMPLOYEE", "MANAGER"), searchTasks);
+router
+    .route("/my-tasks")
+    .get(verifyToken, allowedTo("EMPLOYEE", "MANAGER"), getMyAndTeamTasks);
 
-router.route("/").get(verifyToken, allowedTo("HR"), getTasks);
-router.route("/ongoing").get(verifyToken, allowedTo("HR"), getOngoingTasks);
-router.route("/task/:id").get(verifyToken, allowedTo("HR"), getTasks);
+router.route("/").get(verifyToken, allowedTo("HR", "MANAGER"), getTasks);
+router
+    .route("/ongoing")
+    .get(verifyToken, allowedTo("HR", "MANAGER"), getOngoingTasks);
+router
+    .route("/task/:id")
+    .get(verifyToken, allowedTo("HR", "MANAGER"), getTasks);
 
 router
     .route("/:projectId")
 
     .post(
         verifyToken,
-        allowedTo("HR"),
+        allowedTo("HR", "MANAGER"),
         upload.any(),
         processUploadedFile2,
         setFilesToBody2(),
@@ -47,13 +61,13 @@ router
     .route("/:id")
     .patch(
         verifyToken,
-        allowedTo("HR", "EMPLOYEE"),
+        allowedTo("HR", "EMPLOYEE", "MANAGER"),
         upload.any(),
         processUploadedFile2,
         setFilesToBody2(),
         validate(validateUpdateTaskSchema),
         updateTask
     )
-    .delete(verifyToken, allowedTo("HR"), deleteTask);
+    .delete(verifyToken, allowedTo("HR", "MANAGER"), deleteTask);
 
 export default router;
