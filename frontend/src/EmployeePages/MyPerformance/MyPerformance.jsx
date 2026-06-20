893a3a2293a6eb5e097c69c763cc55faf48fad4c
Menna Ibrahim <mennaibrahim815@gmail.com>
2026-06-20 21:38:04 +0300

    Refactor Myperformance page

--- frontend/src/EmployeePages/MyPerformance/MyPerformance.jsx
+++ frontend/src/EmployeePages/MyPerformance/MyPerformance.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import PerformanceHeader from "../../EmployeeComponents/MyPerformanceComponents/PerformanceHeader";
 import OverallPerformanceCard from "../../EmployeeComponents/MyPerformanceComponents/OverallPerformanceCard";
 import KpiStats from "../../EmployeeComponents/MyPerformanceComponents/KpiStats";
-import PreviousPeriodsChart from "../../EmployeeComponents/MyPerformanceComponents/PreviousPeriodsChart"
+import PreviousPeriodsChart from "../../EmployeeComponents/MyPerformanceComponents/PreviousPeriodsChart";
 import PreviousPeriodsTable from "../../EmployeeComponents/MyPerformanceComponents/PreviousPeriodsTable";
 import { fetchEmployeePerformance } from "../../store/EmployeeSlices/employeePerformance/employeePerformanceSlice";
 
@@ -24,9 +24,9 @@ export default function MyPerformance() {
             <PerformanceHeader />
 
             {loading && (
-                <p style={{ color: "var(--text-muted)" }} className="text-center py-10">
-                    Loading...
-                </p>
+                <div className="flex justify-center items-center py-10">
+                    <i className="fas fa-spinner fa-spin text-4xl text-blue-500"></i>
+                </div>
             )}
 
             {error && (
