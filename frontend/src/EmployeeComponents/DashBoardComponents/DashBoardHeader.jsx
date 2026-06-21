455a5ad23ae1eb37136df80e3425d0ca2d6463c4
Menna Ibrahim <mennaibrahim815@gmail.com>
2026-06-21 04:00:06 +0300

    Resolve import in MyDashboard

--- frontend/src/EmployeeComponents/DashBoardComponents/DashBoardHeader.jsx
+++ frontend/src/EmployeeComponents/DashBoardComponents/DashBoardHeader.jsx
@@ -2,9 +2,8 @@
 
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// استيراد الأكشن لتحديث البيانات بعد إرسال الطلب
 import { setSelectedDate, fetchEmployeeDashboardStats } from "../../store/EmployeeSlices/dashBoard/employeeDashBoardSlice";
-import ReusableCalendar from "../../Components/UI/ReusableCalendar";
+import ReusableCalendar from "../../components/UI/ReusableCalendar";
 import RequestApplicationModel from "../../EmployeeComponents/MyRequestsComponents/RequestsApplicationModel";
 import { Plus } from "lucide-react";
 
